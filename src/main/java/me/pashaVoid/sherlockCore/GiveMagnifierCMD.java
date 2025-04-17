1be646c33bbcc06a20253b1b270c3f1afa87bcb2
Kiyoshi Yeager <tolikadom@gmail.com>
2025-04-17 21:36:00 +0200

    nice work only custom ;)

--- src/main/java/me/pashaVoid/sherlockCore/GiveMagnifierCMD.java
+++ src/main/java/me/pashaVoid/sherlockCore/GiveMagnifierCMD.java
@@ -18,9 +18,10 @@ import java.util.List;
 public class GiveMagnifierCMD implements TabExecutor {
 
     // /giveMagnifier pattern <pattern_name> <player(optional)>
-    // /giveMagnifier custom <uses> <break_chances> <show_time> <add_chances> <show_thief> <player(optional)>
+    // /giveMagnifier custom <nicks> <uses> <break_chances> <show_time> <add_chances> <show_thief> <player(optional)>
 
     // magnifier: True
+    // magnifier_nicks: int
     // magnifier_uses: int
     // magnifier_break_chances: int
     // magnifier_show_time: int
@@ -44,7 +45,7 @@ public class GiveMagnifierCMD implements TabExecutor {
         Player getterPlayer = null;
         if (pattern && args.length >= 3) {
             getterPlayer = Bukkit.getPlayer(args[2]);
-        } else if (!pattern && args.length >= 7){
+        } else if (!pattern && args.length >= 8){
             getterPlayer = Bukkit.getPlayer(args[6]);
         } else if (sender instanceof Player) {
             getterPlayer = (Player) sender;
@@ -59,16 +60,18 @@ public class GiveMagnifierCMD implements TabExecutor {
             return true;
 
         } else {
-            int uses = Integer.parseInt(args[1]);
-            int break_chances = Integer.parseInt(args[2]);
-            int show_time = Integer.parseInt(args[3]);
-            int add_chances = Integer.parseInt(args[4]);
-            int show_thief = Integer.parseInt(args[5]);
+            int nicks = Integer.parseInt(args[1]);
+            int uses = Integer.parseInt(args[2]);
+            int break_chances = Integer.parseInt(args[3]);
+            int show_time = Integer.parseInt(args[4]);
+            int add_chances = Integer.parseInt(args[5]);
+            int show_thief = Integer.parseInt(args[6]);
 
             ItemStack item = new ItemBuilder(Material.SHEARS, 1)
                     .setName("Лупа Шерлока")
                     .setLore(Arrays.asList("ЛКМ - увидеть историю блока", "SHIFT + ЛКМ - увидеть историю блока выше", "ЛКМ - увидеть историю содержимого блока", "ЛКМ - увидеть историю содержимого блока выше"))
                     .addPersistent("magnifier", PersistentDataType.BOOLEAN, true)
+                    .addPersistent("magnifier_nicks", PersistentDataType.INTEGER, nicks)
                     .addPersistent("magnifier_uses", PersistentDataType.INTEGER, uses)
                     .addPersistent("magnifier_break_chances", PersistentDataType.INTEGER, break_chances)
                     .addPersistent("magnifier_show_time", PersistentDataType.INTEGER, show_time)
--- src/main/java/me/pashaVoid/sherlockCore/ItemListener.java
+++ src/main/java/me/pashaVoid/sherlockCore/ItemListener.java
@@ -2,7 +2,7 @@ package me.pashaVoid.sherlockCore;
 
 
 import org.bukkit.Bukkit;
-import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Player;
@@ -11,6 +11,8 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.persistence.PersistentDataContainer;
+import org.bukkit.persistence.PersistentDataType;
 
 import java.util.List;
 
@@ -24,21 +26,32 @@ public class ItemListener implements Listener {
         if (item == null) {
             return;
         }
-        else if (!(e.getItem().getType() == Material.SHEARS)) {
+        if (item.getItemMeta() == null) {
             return;
         }
-        else if (e.getAction() != Action.LEFT_CLICK_BLOCK) {
+        if (!item.getItemMeta().getPersistentDataContainer().has(NamespacedKey.minecraft("magnifier"))) {
             return;
         }
-        else if (e.getClickedBlock() == null) {
-            return;
-        }
+
         e.setCancelled(true);
+
+        if (e.getAction() != Action.LEFT_CLICK_BLOCK && e.getAction() != Action.RIGHT_CLICK_BLOCK) {
+            return;
+        }
+        if (e.getClickedBlock() == null) {
+            return;
+        }
+        PersistentDataContainer container = e.getItem().getItemMeta().getPersistentDataContainer();
+        int nicks = container.get(NamespacedKey.minecraft("magnifier_nicks"), PersistentDataType.INTEGER);
+        int uses = container.get(NamespacedKey.minecraft("magnifier_uses"), PersistentDataType.INTEGER);
+        int break_chances = container.get(NamespacedKey.minecraft("magnifier_break_chances"), PersistentDataType.INTEGER);
+        int show_time = container.get(NamespacedKey.minecraft("magnifier_show_time"), PersistentDataType.INTEGER);
+        int add_chances = container.get(NamespacedKey.minecraft("magnifier_add_chances"), PersistentDataType.INTEGER);
+        int show_thief = container.get(NamespacedKey.minecraft("magnifier_show_thief"), PersistentDataType.INTEGER);
 
         Player player = e.getPlayer();
 
         Block targetBlock = e.getClickedBlock(); // блок на который тыкнул
-        // Если на шифте, то берется блок сверху
         if (player.isSneaking()) {
             targetBlock = targetBlock.getRelative(BlockFace.UP);
         }
@@ -46,18 +59,30 @@ public class ItemListener implements Listener {
         Block block = targetBlock; // выбираем финальный блок
 
         Bukkit.getScheduler().runTaskAsynchronously(SherlockCore.getInstance(), () -> {
-            List<List<String>> history = getBlockHistory(block, 10);
+            List<List<String>> history;
+            if (e.getAction() == Action.LEFT_CLICK_BLOCK) {
+                history = getBlockHistory(block, nicks);
+            } else {
+                history = getBlockHistory(block, uses);
+                player.sendMessage("ПКМ еще не готов");
+            }
 
             if (history.isEmpty()) {
                 player.sendMessage("§aУ этого блока нет истории изменений.");
                 return;
             }
 
-            player.sendMessage("§6История блока §f" + block.getType().toString().toLowerCase() + ":");
+            player.sendMessage("§6Вот что удалось разглядеть:");
             int cnt = 0;
+            int time_cnt = show_time;
             for (List<String> entry : history) {
                 cnt += 1;
-                player.sendMessage("§7" + cnt + "§f " + entry.get(0) + " §7→ " + entry.get(2)+ " §8(" + entry.get(1) + ")");
+                String str = "§7" + cnt + "§f " + entry.get(0) + " §7→ " + entry.get(2);
+                if (time_cnt > 0) {
+                    str += " §8(" + entry.get(1) + ")";
+                    time_cnt -= 1;
+                }
+                player.sendMessage(str);
             }
         });
     }
